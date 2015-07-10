efdc6bd2ca59d9595cc001cd2246ad7ebeae8213
MGNute <mike.nute@gmail.com>
2015-07-10 14:39:54 -0500

    adding new files

---
+++ alignment_utils.py
@@ -0,0 +1,331 @@
+#-------------------------------------------------------------------------------
+# Name:        alignment_utils.py
+# Purpose:
+#
+# Author:      Michael
+#
+# Created:     10/02/2015
+# Copyright:   (c) Michael 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import platform, os, StringIO
+import re
+import dendropy
+# import numpy as np
+# import pasta_output_readers as pouts
+# import utilities
+
+
+def fastsp_run_on_two_fastas(ref_file,est_file,out=None):
+    import subprocess
+
+    if out==None:
+        out=open('temp.txt','w')
+    else:
+        out=open(out,'w')
+    subprocess.call(["java","-jar","/home/mikenute/Phylolab/share/code/misc/FastSP_1.6.0.jar","-r",
+        ref_file,"-e",est_file], stdout=out,stderr=out)
+    out.close()
+    out = open('temp.txt','r')
+    text=out.read()
+    print text
+    out.close()
+
+    # text=out.getvalue()
+    # p,f=os.path.split(ref_file)
+    # p2,f2=os.path.split(est_file)
+    args=fastsp_read_results_string(text)
+    args['ref_file']=ref_file
+    args['est_file']=est_file
+    os.unlink('temp.txt')
+    return args
+
+def fastsp_read_results_file(filepath=None,optional_filename=None):
+    pa, fi = os.path.split(filepath)
+    myargs = {}
+    if optional_filename<>None:
+        fi=optional_filename
+    # myargs['file_name'] = fi
+
+    # open the results file
+    textfile = open(filepath, 'r')
+    text = textfile.read()
+    textfile.close()
+
+    args=fastsp_read_results_string(text)
+    args['file_name']=fi
+    return args
+
+
+def fastsp_read_results_string(text, ref_file=None, est_file=None):
+    import re
+    myargs = {}
+
+    # open the results file
+    # textfile = open(filepath, 'r')
+    # text = textfile.read()
+    # textfile.close()
+
+    # regexes to pull the results
+    file_regex = 'SP-Score (?P<sp>\d+\.\d+[E\-\d]*)[.\n]*Modeler (?P<modeler>\d+\.\d+[E\-\d]*)[.\n]*SPFN (?P<spfn>\d+\.\d+[E\-\d]*)[.\n]*SPFP (?P<spfp>\d+\.\d+[E\-\d]*)[.\n]*Compression (?P<comp>\d+\.\d+[E\-\d]*)[.\n]*TC (?P<tc>\d+\.\d+[E\-\d]*)'
+    file_regex_2 = 'MaxLenNoGap= (?P<maxlen>\d+).*NumSeq= (?P<numseq>\d+).*LenRef= (?P<lenref>\d+).*LenEst= (?P<lenest>\d+).*Cells= (?P<cells>\d+)'
+    fileregs = re.compile(file_regex)
+    fileregs2 = re.compile(file_regex_2)
+
+    vals1list = ['sp', 'modeler', 'spfn', 'spfp', 'comp', 'tc']
+    vals2list = ['maxlen', 'numseq', 'lenref', 'lenest', 'cells']
+    vals1 = fileregs.search(text)
+    vals2 = fileregs2.search(text)
+    for i in vals1list:
+        try:
+            myargs[i] = vals1.group(i)
+        except:
+            myargs[i] = ''
+    for i in vals2list:
+        try:
+            myargs[i] = vals2.group(i)
+        except:
+            myargs[i] = ''
+    return myargs
+
+def fastsp_results_folder_to_tab_delimited(folder=None, output_file_path=None):
+    # quick checks on folder string
+    folder = folder.replace('\\', '/') + '/'
+    folder = folder.replace('//', '/')
+
+    # model output line:
+    line_str = '%(file_name)s\t%(sp)s\t%(modeler)s\t%(spfn)s\t%(spfp)s\t%(comp)s\t%(tc)s\t%(maxlen)s\t%(numseq)s\t%(lenref)s\t%(lenest)s\t%(cells)s\t\n'
+
+    outfile = open(output_file_path, 'w')
+    outfile.write(
+        'file_name\tSP-Score\tModeler\tSPFN\tSPFP\tCompression Ratio\tTC\tMaxLenNoGap\tNumSeq\tLenRef\tLenEst\tCells\t\n')
+
+    for i in os.listdir(folder):
+        myargs = {}
+        myargs = fastsp_read_results_file(folder + i)
+        outfile.write(line_str % myargs)
+
+    outfile.close()
+
+def read_from_fasta(self,file_path):
+        output={}
+        fasta=open(file_path,'r')
+        first=True
+        for l in fasta:
+            if l[0]=='>':
+                if first<>True:
+                    output[name]=seq
+                else:
+                    first=False
+                name=l[1:].strip()
+                seq=''
+            else:
+                seq=seq + l.strip()
+        output[name]=seq
+        fasta.close()
+        return output
+
+def write_to_fasta(out_file_path=None,fasta_dict=None,subset_keys=None,raw=False):
+
+    if subset_keys==None:
+        mykeys=fasta_dict.keys()
+    else:
+        mykeys=subset_keys
+
+    fasta=open(out_file_path,'w')
+    for i in mykeys:
+        fasta.write('>'+i+'\n')
+        if raw==False:
+            fasta.write(fasta_dict[i]+'\n')
+        else:
+            fasta.write(fasta_dict[i].replace('-','')+'\n')
+
+    fasta.close()
+
+class alignment_from_fasta:
+    '''
+    This is a clumsy class that represents a fasta file. It could easily be written as a smaller set of
+    functions (and has been in another file) but this works for now.
+    '''
+    def __init__(self):
+        self.taxa={}
+        self.filename = None
+        self.folder = None
+        self.path = None
+
+    def assign_fasta(self,file_path):
+        self.sequence=self.read_from_fasta(file_path)
+        self.folder, self.filename = os.path.split(file_path)
+        self.path = file_path
+
+    def read_from_fasta(self,file_path):
+        output={}
+        fasta=open(file_path,'r')
+        first=True
+        for l in fasta:
+            if l[0]=='>':
+                if first<>True:
+                    output[name]=seq
+                else:
+                    first=False
+                name=l[1:].strip()
+                seq=''
+            else:
+                seq=seq + l.strip()
+        output[name]=seq
+        fasta.close()
+        return output
+
+    def write_to_fasta(self,out_file_path=None,fasta_dict=None,subset_keys=None,raw=False):
+        if fasta_dict==None:
+            fasta_dict=self.sequence
+
+        if subset_keys==None:
+            mykeys=fasta_dict.keys()
+        else:
+            mykeys=subset_keys
+
+        fasta=open(out_file_path,'w')
+        for i in mykeys:
+            fasta.write('>'+i+'\n')
+            if raw==False:
+                fasta.write(fasta_dict[i]+'\n')
+            else:
+                fasta.write(fasta_dict[i].replace('-','')+'\n')
+
+        fasta.close()
+
+    def count_all_blank_columns(self):
+        num_seqs=len(self.sequence.values())
+        seq_len=len(self.sequence.values()[0])
+        print num_seqs
+        print seq_len
+        import StringIO
+
+        record=StringIO.StringIO()
+        record.write('line\tblank\toccupied\n')
+
+        for i in self.sequence.keys():
+            if len(self.sequence[i])<>seq_len:
+                print i + ' has sequence length different from the first'
+                print len(self.sequence[i])
+
+        for i in range(seq_len):
+            all_blank=True
+            blank_count=0
+            occ_count=0
+            for j in self.sequence.keys():
+                if self.sequence[j][i]<>'-':
+                    occ_count+=1
+                else:
+                    blank_count +=1
+            record.write('%s\t%s\t%s\n' % (i, blank_count, occ_count))
+
+        return record
+
+    def remove_all_blank_columns(self,same_length_check=True):
+        num_seqs=len(self.sequence.values())
+        seq_len = len(self.sequence.values()[0])
+
+        if same_length_check==True:
+            for i in self.sequence.values():
+                assert len(i) == seq_len
+
+        all_blanks_list = []
+        for i in range(seq_len):
+            allblank=True
+            for j in self.sequence.values():
+                if j[i]<>'-':
+                    allblank=False
+                    break
+            if allblank==True:
+                all_blanks_list.append(i)
+
+        if len(all_blanks_list)>0:
+            all_blanks_list.sort(reverse=True)
+            # for j in self.sequence.values():
+            #     j = list(j)
+
+            for i in all_blanks_list:
+                for j in self.sequence.values():
+                    j = j[0:i-1] + j[i:]
+            #
+            # for j in self.sequence.values():
+            #     j = ''.join(j)
+
+
+
+
+    def get_sequence_count(self):
+        return len(self.sequence.keys())
+
+    # def unmask_fasta_subset(self,fasta_dict,subset_keys):
+    #     new_fasta={}
+    #     vals=[]
+    #     for i in subset_keys:
+    #         new_fasta[i]=''
+    #         vals.append(fasta_dict[i])
+    #
+    #     k=max(map(len,vals))
+    #     for j in range(k):
+
+
+def get_prefix(os=None):
+    if os == None:
+        os = platform.system()
+    prefix_general_windows = 'C:/Users/Michael/Grad School Stuff/Research/Phylogenetics'
+    prefix_general_linux = '/home/mikenute/Phylolab/share'
+    if os == 'Windows':
+        prefix_general = prefix_general_windows
+    else:
+        prefix_general = prefix_general_linux
+    return prefix_general
+
+def reduce_file(ref_file, in_file, reduced_file):
+    pref=get_prefix()
+    # file1=pref+'/results/homfam/pasta_results/aln_full/homfam_aat__200.marker001.homfam_aat__raw.aln'
+    file1=pref+in_file
+    # file_ref=pref+'/Data/homfam/aat/model/true.reduced.fasta'
+    file_ref=pref+ref_file
+    # file_out=pref+'/results/homfam/pasta_results/aln_reduced/homfam_aat__200.marker001.homfam_aat.aln'
+    file_out=pref+reduced_file
+    aln=alignment_from_fasta()
+    fasta1=aln.read_from_fasta(file1)
+    fasta_ref=aln.read_from_fasta(file_ref)
+
+    a=set(fasta1.keys()).intersection(fasta_ref.keys())
+    aln.write_to_fasta(file_out,fasta1,a)
+    for i in a:
+        print i + ' - ' + str(len(fasta_ref[i])) + ' - ' + str(len(fasta1[i]))
+    print 'done: ' + in_file
+
+def fastsp_get_empty_args():
+    a= {
+        'file_name':None,
+        'sp':None,
+        'modeler':None,
+        'spfn':None,
+        'spfp':None,
+        'comp':None,
+        'tc':None,
+        'maxlen':None,
+        'numseq':None,
+        'lenref':None,
+        'lenest':None,
+        'cells':None
+    }
+    return a
+
+if __name__ == '__main__':
+    # zz_test_nams_fasta_files_for_blanks()
+    myf=get_prefix()+'/code/sepp/test/unittest/data/upp/initial.fas'
+    aln=alignment_from_fasta()
+    aln.assign_fasta(myf)
+    print aln.get_sequence_count()
+    # print aln.sequence.keys()
+
+
+
+
+
+
---
+++ baliphy_raw_dispatcher.py
@@ -0,0 +1,90 @@
+__author__ = 'Michael'
+
+import subprocess, sys
+import multiprocessing
+# from alignment_utils import get_prefix
+
+def main(myfrom=None, myto=None, suffix='', inputlistfilename=None, proc=16):
+    # a=read_list_from_file(get_prefix() + '/results/2015_04_20_baliphy_in_UPP/input_fasta_list.txt')
+    if inputlistfilename==None:
+        a=read_list_from_file()
+    else:
+        a=read_list_from_file(inputlistfilename)
+    # print myfrom
+    # print len(a[myfrom:myto])
+    # print multiprocessing.cpu_count()
+
+    p=multiprocessing.Pool(proc)
+    args=zip(a[myfrom:myto],[suffix]*len(a[myfrom:myto]))
+    p.map(run_baliphy_intermediate,args)
+    pass
+
+def run_baliphy_intermediate(args):
+    run_baliphy(*args)
+
+def run_baliphy(set, suffix=''):
+    a=set.split('/')[1:4]
+    # output_path='/scratch/users/nute2/results/baliphy_upp/' + '_'.join(a)+ '_' + suffix
+    # print output_path
+    output_path = '/projects/sciteam/jtr/work/nute/baliphy_upp/output/' + '_'.join(a)
+
+    arg1='bali-phy'
+    # arg2='~/data/rosedna/100S1/' + set + '.fasta'
+    arg2=set
+    # arg3='--config'
+    # arg4='100S1_' + set + '_raw.txt'
+    arg5= '--builtins-path'
+    # arg6='/home/nute2/Phylolab/bali-phy/bali-phy-2.3.5/lib/bali-phy/'
+    arg6 = '/projects/sciteam/jtr/programs/bali-phy/bali-phy-2.3.6/lib/bali-phy/'
+    arg7='--modules-path'
+    # arg8='/home/nute2/Phylolab/bali-phy/bali-phy-2.3.5/lib/bali-phy/modules/'
+    arg8='/projects/sciteam/jtr/programs/bali-phy/bali-phy-2.3.6/lib/bali-phy/modules/'
+    arg9='--name'
+    arg10 = output_path
+    # subprocess.call([arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10])
+    subprocess.call([arg1, arg2, arg5, arg6, arg7, arg8, arg9, arg10])
+
+def read_list_from_file(filename='/home/nute2/baliphy_upp_work/input_fasta_list.txt'):
+    mylist = open(filename,'r')
+    outlist=[]
+    for i in mylist:
+        outlist.append(i.strip())
+
+    return outlist
+
+def print_help():
+    print """
+    baliphy_raw_dispatcher.py dispatches baliphy to run baliphy on a set of files from a list.
+    USAGE: python baliphy_raw_dispatcher [from] [to] (additional arguments)
+        [from]: the entry of the list to start AFTER (so if you want to run baliphy on entries 17 to 32, [from] should be 16).
+        [to]:   the entry of the list to stop ON (so in the previous example, this should be 32).
+
+    Additional Optional Arguments:
+        --suffix [suffix]:      a suffix to append to the output folders
+        --filename [filename]:  the path to a specific list of inputs to use (defaults to 'input_fasta_list.txt')
+        --proc [proc]:          number of processes to spawn. Ideally this should match the number of processors available.
+        --help:     displays the help section (this text)
+    """
+
+if __name__=='__main__':
+    if '--help' not in sys.argv:
+        myfrom = int(sys.argv[1].strip())
+        myto = int(sys.argv[2].strip())
+        if '--suffix' in sys.argv:
+            suffix = sys.argv[sys.argv.index('--suffix')+1]
+        else:
+            suffix= ''
+
+        if '--filename' in sys.argv:
+            filename = sys.argv[sys.argv.index('--filename')+1]
+
+        if '--proc' in sys.argv:
+            proc=int(sys.argv[sys.argv.index('--proc')+1])
+        else:
+            proc=16
+
+        # print myfrom
+        # print myto
+        main(myfrom, myto, suffix,filename,proc)
+    else:
+        print_help()
